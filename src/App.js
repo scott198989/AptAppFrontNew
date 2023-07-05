3b0a9943db48bfa7773f1207e0ac31c4bb25a656
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-07-04 20:52:36 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ApartmentIndex from './pages/ApartmentIndex';
 import ApartmentShow from './pages/ApartmentShow';
@@ -11,157 +11,94 @@ import ApartmentProtectedIndex from './pages/ApartmentProtectedIndex'
 import NotFound from './pages/NotFound';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
-import ApartmentDelete from './pages/ApartmentDelete'
-import AptMock from './AptMock';
-import UserMock from './UserMock';
+import ApartmentDelete from './pages/ApartmentDelete';
 
-
-
-const App = (props) => {
+const App = () => {
   const [apartments, setApartments] = useState([]);
-  // const [user, setUser] = useState(UserMock);
   const [currentUser, setCurrentUser] = useState(null);
- 
-  const url = "https://apartment-app-backend-gejb.onrender.com"
+  const url = "https://apartment-app-backend-gejb.onrender.com";
 
   useEffect(() => {
-    readApts()
-  }, [])
+    readApts();
+    const loggedInUser = localStorage.getItem("token");
+    if (loggedInUser) {
+      setCurrentUser(loggedInUser);
+    }
+  }, []);
 
-  // apartment fetches
+  // Fetch apartments
   const readApts = () => {
     fetch(`${url}/apartments`)
       .then(response => response.json())
       .then(payload => {
-        setApartments(payload)
+        setApartments(payload);
       })
-      .catch((error) => console.log(error))
-  }
+      .catch((error) => console.log(error));
+  };
+
   const createApartment = (apartment) => {
-    // console.log(apartment);
     fetch(`${url}/apartments`, {
-      body: JSON.stringify(createdApartment),
+      body: JSON.stringify(apartment),
       headers: {
         "Content-Type": "application/json"
       },
       method: "POST"
     })
       .then((response) => response.json())
-      .then((payload) => readApts())
+      .then(() => readApts())
       .catch((errors) => console.log("Apartment create errors:", errors));
   };
-  
-
-   // authentication function
-   const login = (userInfo) => {
-    console.log("Login user info on App.js", userInfo)
-    fetch(`${url}/login`, {
-      body: JSON.stringify(userInfo),
-      headers: {
-        "Content-Type": 'application/json',
-        "Accept": 'application/json'
-      },
-      method: 'POST'
-    })
-      .then(response => {
-        if(!response.ok) {
-          throw Error(response.statusText)
-        }
-        localStorage.setItem("token", response.headers.get("Authorization"))
-        return response.json()
-      })
-      .then(payload => {
-        setCurrentUser(payload)
-      })
-      .catch(error => console.log("login errors: ", error))
-  } 
-
-  const logout = () => {
-    fetch(`${url}/logout`, {
-      headers: {
-        "Content-Type": 'application/json',
-        "Authorization": localStorage.getItem("token") //retrieve the token 
-      },
-      method: 'DELETE'
-    })
-      .then(payload => {
-        localStorage.removeItem("token")  // remove the token
-        setCurrentUser(null)
-      })
-      .catch(error => console.log("log out errors: ", error))
-  }
-
-  const signup = (userInfo) => {
-    fetch(`${url}/signup`, {
-      body: JSON.stringify(userInfo),
-      headers: {
-        "Content-Type": 'application/json',
-        "Accept": 'application/json'
-      },
-      method: 'POST'
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw Error(response.statusText)
-        }
-        // store the token
-        localStorage.setItem("token", response.headers.get("Authorization"))
-        return response.json()
-      })
-      .then(payload => {
-        setCurrentUser(payload)
-      })
-      .catch(error => console.log("login errors: ", error))
-  }
 
   const deleteApartment = (id) => {
-    fetch(`https://apartment-app-backend-gejb.onrender.com/${id}`, {
+    fetch(`${url}/apartments/${id}`, {
       headers: {
         "Content-Type": "application/json"
       },
       method: "DELETE"
     })
-      .then((response) => response.json())
-      .then((payload) => readApts())
-      .catch((errors) => console.log("delete errors:", errors))
-  }
+      .then(() => readApts())
+      .catch((errors) => console.log("Delete errors:", errors));
+  };
 
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem("token")
-    if(loggedInUser) {
-      setCurrentUser(loggedInUser)
-    }
-    readApts()
-  }, [])
-  
   return (
     <div>
-    <Header currentUser={currentUser} logout={logout}/>
+      <Header currentUser={currentUser} />
       <Routes>
-        <Route path="/" element={<Home apartments={apartments} />
-} /> 
-        <Route path="/ApartmentIndex" element={<ApartmentIndex  apartments={aptsMock}/>} />
-        <Route path="/ApartmentShow/:id" element={<ApartmentShow aptsMock={aptsMock} deleteApartment={deleteApartment}/>} />
-        <Route path="/login" element={<LogIn login={login}/>} />
-        <Route path="/SignUp" element={<SignUp signup={signup}/>} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/apartmentIndex"
+          element={<ApartmentIndex apartments={apartments} />}
+        />
+        <Route
+          path="/apartmentShow/:id"
+          element={<ApartmentShow apartments={apartments} />}
+        />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
         {currentUser && (
-        <Route path="/API" 
-              element={
-              <ApartmentProtectedIndex 
-              currentUser = {currentUser}
-              apartments={apartments}
+          <Route
+            path="/api"
+            element={
+              <ApartmentProtectedIndex
+                currentUser={currentUser}
+                apartments={apartments}
               />
-              } 
-            />
-          )}
-         <Route path="/ApartmentNew" element={<ApartmentNew createApartment={createApartment}/>} />
-        <Route path="/ApartmentEdit" element={<ApartmentEdit updateApts={updateApts} currentUser={currentUser}/>} />
+            }
+          />
+        )}
+        <Route
+          path="/apartmentNew"
+          element={<ApartmentNew createApartment={createApartment} />}
+        />
+        <Route
+          path="/apartmentEdit/:id"
+          element={<ApartmentEdit apartments={apartments} />}
+        />
         <Route path="*" element={<NotFound />} />
-        <Route path="/ApartmentShow/:id"element={<ApartmentShow apartments={apartments} deleteApartment={deleteApartment} logged_in={props.logged_in} currentUser={props.current_user}  />}/>
       </Routes>
-    <Footer />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
